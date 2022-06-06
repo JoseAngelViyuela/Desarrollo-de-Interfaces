fc74045d71ea1a5819da5bb0fbc6e789d7e28949
JoseAngelViyuela <55293691+JoseAngelViyuela@users.noreply.github.com>
2022-06-06 21:24:35 +0200

    Cuarto commit

--- Práctica Extraordinaria junio Desarrollo de Interfaces/src/application/Main.java
+++ Práctica Extraordinaria junio Desarrollo de Interfaces/src/application/Main.java
@@ -1,15 +1,36 @@
 package application;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import model.Trabajador;
 import view.MenuController;
 
 public class Main extends Application {
+
     private Stage primaryStage;
 	private BorderPane rootLayout;
+
+	private ObservableList<Trabajador> trabajadorData = FXCollections.observableArrayList();
+
+	public Main() {
+		trabajadorData.add(new Trabajador("Antonio", "García"));
+		trabajadorData.add(new Trabajador("María Carmen", "Rodríguez"));
+		trabajadorData.add(new Trabajador("María", "González"));
+		trabajadorData.add(new Trabajador("Manuel", "Fernández"));
+		trabajadorData.add(new Trabajador("José", "López"));
+		trabajadorData.add(new Trabajador("Francisco", "Martínez"));
+		trabajadorData.add(new Trabajador("Carmen", "Sánchez"));
+		trabajadorData.add(new Trabajador("David", "Pérez"));
+	}
+
+	public ObservableList<Trabajador> getTrabajadorData() {
+		return trabajadorData;
+	}
 
 	@Override
 	public void start(Stage primaryStage) {
---
+++ Práctica Extraordinaria junio Desarrollo de Interfaces/src/view/TrabajadorOverviewController.java
@@ -0,0 +1,85 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import application.Main;
+import model.Trabajador;
+import util.DateUtil;
+
+public class TrabajadorOverviewController {
+
+	@FXML
+	private TableView<Trabajador> trabajadorTable;
+    @FXML
+    private TableColumn<Trabajador, String> firstNameColumn;
+    @FXML
+    private TableColumn<Trabajador, String> lastNameColumn;
+
+    @FXML
+    private Label firstNameLabel;
+    @FXML
+    private Label lastNameLabel;
+    @FXML
+    private Label streetLabel;
+    @FXML
+    private Label postalCodeLabel;
+    @FXML
+    private Label cityLabel;
+    @FXML
+    private Label birthdayLabel;
+
+    // Reference to the main application.
+    private Main mainApp;
+
+    public TrabajadorOverviewController() {
+    }
+
+    @FXML
+    private void initialize() {
+    	// Initialize the person table with the two columns.
+        firstNameColumn.setCellValueFactory(
+                cellData -> cellData.getValue().firstNameProperty());
+        lastNameColumn.setCellValueFactory(
+                cellData -> cellData.getValue().lastNameProperty());
+
+        // Clear person details.
+        showPersonDetails(null);
+
+        // Listen for selection changes and show the person details when changed.
+        trabajadorTable.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> showPersonDetails(newValue));
+    }
+
+    public void setMain(Main mainApp) {
+        this.mainApp = mainApp;
+
+        // Add observable list data to the table
+        trabajadorTable.setItems(mainApp.getTrabajadorData());
+    }
+
+    private void showPersonDetails(Trabajador trabajador) {
+        if (trabajador != null) {
+            // Fill the labels with info from the person object.
+            firstNameLabel.setText(trabajador.getFirstName());
+            lastNameLabel.setText(trabajador.getLastName());
+            streetLabel.setText(trabajador.getStreet());
+            postalCodeLabel.setText(Integer.toString(trabajador.getPostalCode()));
+            cityLabel.setText(trabajador.getCity());
+            birthdayLabel.setText(DateUtil.format(trabajador.getBirthday()));
+            // TODO: We need a way to convert the birthday into a String!
+            // birthdayLabel.setText(...);
+        } else {
+            // Person is null, remove all the text.
+            firstNameLabel.setText("");
+            lastNameLabel.setText("");
+            streetLabel.setText("");
+            postalCodeLabel.setText("");
+            cityLabel.setText("");
+            birthdayLabel.setText("");
+        }
+    }
+
+
+}
