fc233982ac94ea0c78dd09b8e8dbd4f1cab22712
JoseAngelViyuela <55293691+JoseAngelViyuela@users.noreply.github.com>
2022-06-13 23:32:05 +0200

    Undécimo commit

--- Práctica Extraordinaria junio Desarrollo de Interfaces/src/view/TutorialController.java
+++ Práctica Extraordinaria junio Desarrollo de Interfaces/src/view/TutorialController.java
@@ -16,7 +16,7 @@ public class TutorialController {
 
     @FXML
     private void initialize() {
-    	tutorialDatos.setText("En la pantalla de Datos se encuentra la tabla con los nombres y apellidos de los trabajadores los cuales podemos seleccionar con un click para ver el resto de información");
+    	tutorialDatos.setText("En la pantalla de Datos se encuentra la tabla con el nombre de los trabajadores los cuales podemos ojear con un click para ver el resto de datos");
     	tutorialAgnadir.setText("Podemos añadir datos en el botón de añadir e introduciendo los valores que consideremos y pulsar el botón de confirmación");
     	tutorialEditar.setText("Para editar un trabajador tendremos que seleccionarlo con un click y cambiar los valores que consideremos");
     	tutorialEliminar.setText("Para eliminar el dato de un trabajador hay que seleccionarlo con un click, pero hay que tener cuidado porque perderemos los datos");
